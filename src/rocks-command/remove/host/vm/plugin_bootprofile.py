7ef5a2a0701d7a64c8dff494d1abb9a1d966fd32
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2008-09-02 18:03:16 +0000

    support plugin to remove host-specific bootprofile when removing vm host

--- src/rocks-command/remove/host/vm/plugin_bootprofile.py
+++ src/rocks-command/remove/host/vm/plugin_bootprofile.py
@@ -1,5 +1,5 @@
-# $Id: __init__.py,v 1.7 2008/09/02 18:03:16 phil Exp $
-#
+# $Id: plugin_bootprofile.py,v 1.1 2008/09/02 18:03:16 phil Exp $
+# 
 # @Copyright@
 # 
 # 				Rocks(r)
@@ -53,109 +53,30 @@
 # 
 # @Copyright@
 #
-# $Log: __init__.py,v $
-# Revision 1.7  2008/09/02 18:03:16  phil
+# $Log: plugin_bootprofile.py,v $
+# Revision 1.1  2008/09/02 18:03:16  phil
 # support plugin to remove host-specific bootprofile when removing vm host
 #
-# Revision 1.6  2008/04/21 16:37:35  bruno
-# nuked the vm_macs table -- now using the networks table to store/retrieve
-# mac addresses for VMs
-#
-# Revision 1.5  2008/04/17 16:38:21  bruno
-# incorporate phil's vm changes
-#
-# Revision 1.4  2008/03/06 23:42:04  mjk
+# Revision 1.2  2008/03/06 23:42:04  mjk
 # copyright storm on
 #
-# Revision 1.3  2008/02/01 21:38:54  bruno
-# closer
-#
-# Revision 1.2  2008/02/01 21:27:53  bruno
+# Revision 1.1  2008/02/01 21:27:53  bruno
 # plugin for removing VM configuration info from the database
 #
-# Revision 1.1  2007/12/03 19:48:51  bruno
-# xen for V
+# Revision 1.1  2008/02/01 20:52:27  bruno
+# use plugins to support removing all database entries for a host.
 #
 #
 
 import os
 import rocks.commands
 
-class Command(rocks.commands.remove.host.command):
-	"""
-	Remove the configuration info in the database for the supplied hosts.
+class Plugin(rocks.commands.Plugin):
 
-	<arg type='string' name='host' repeat='1'>
-	A list of one or more VM host names.
-	</arg>
+	def provides(self):
+		return 'bootprofile'
 
-	<example cmd='remove host vm compute-0-0-0'>
-	Remove the configuration info in the database for compute-0-0-0.
-	</example>
-	"""
-
-	def run(self, params, args):
-		if not len(args):
-			self.abort('must supply at least one host')
-			
-                self.runPlugins(args)
-
-		for host in self.getHostnames(args):
-			vmnodeid = None
-			mem = None
-			cpus = None
-			macs = None
-			disks = None
-
-			#
-			# get the node id of the VM
-			#
-			rows = self.db.execute("""select vn.id from
-				nodes n, vm_nodes vn where vn.node = n.id
-				and n.name = '%s' """ % host)
-			
-			if rows != 1:
-				continue
-
-			vmnodeid, = self.db.fetchone()
-
-			if not vmnodeid:
-				continue
-
-			#
-			# get the name of the physical node that hosts
-			# this VM
-			#
-			rows = self.db.execute("""select vn.physnode from
-				vm_nodes vn, nodes n where n.name = '%s'
-				and n.id = vn.node""" % (host))
-
-			if rows == 1:
-				physnodeid, = self.db.fetchone()
-			else:
-				continue
-
-			rows = self.db.execute("""select name from nodes where
-				id = %s""" % (physnodeid))
-
-			if rows == 1:
-				physhost, = self.db.fetchone()
-			else:
-				continue
-
-			#
-			# remove the configuration file on the physical node
-			#
-			os.system('ssh -q %s "rm -f /etc/xen/rocks/%s"' % 
-				(physhost, host))
-
-			#
-			# now remove the relevant rows in the database for
-			# this VM
-			#
-			self.db.execute("""delete from vm_nodes where
-				id = %s""" % (vmnodeid))
-
-			self.db.execute("""delete from vm_disks where
-				vm_node = %s""" % (vmnodeid))
-
+	def run(self, args):
+		if len(args) > 0:
+			self.owner.command('remove.host.vm.bootprofile', [ args ])
+		
