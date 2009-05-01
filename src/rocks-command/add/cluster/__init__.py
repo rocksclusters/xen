76307736ab0dc08d1fe5c2067f1d842dd7941c7c
Mason Katz <mason.katz@gmail.com>
2009-05-01 19:07:33 +0000

    chimi con queso

--- src/rocks-command/add/cluster/__init__.py
+++ src/rocks-command/add/cluster/__init__.py
@@ -1,12 +1,12 @@
-# $Id: __init__.py,v 1.18 2009/04/22 17:52:28 bruno Exp $
+# $Id: __init__.py,v 1.19 2009/05/01 19:07:33 mjk Exp $
 # 
 # @Copyright@
 # 
 # 				Rocks(r)
 # 		         www.rocksclusters.org
-# 		           version 5.1  (VI)
+# 		       version 5.2 (Chimichanga)
 # 
-# Copyright (c) 2000 - 2008 The Regents of the University of California.
+# Copyright (c) 2000 - 2009 The Regents of the University of California.
 # All rights reserved.	
 # 
 # Redistribution and use in source and binary forms, with or without
@@ -54,6 +54,9 @@
 # @Copyright@
 #
 # $Log: __init__.py,v $
+# Revision 1.19  2009/05/01 19:07:33  mjk
+# chimi con queso
+#
 # Revision 1.18  2009/04/22 17:52:28  bruno
 # allow the user to set the size of the frontend disk size.
 #
--- src/rocks-command/report/host/vm/__init__.py
+++ src/rocks-command/report/host/vm/__init__.py
@@ -1,12 +1,12 @@
-# $Id: __init__.py,v 1.41 2009/04/14 16:12:17 bruno Exp $
+# $Id: __init__.py,v 1.42 2009/05/01 19:07:34 mjk Exp $
 # 
 # @Copyright@
 # 
 # 				Rocks(r)
 # 		         www.rocksclusters.org
-# 		           version 5.1  (VI)
+# 		       version 5.2 (Chimichanga)
 # 
-# Copyright (c) 2000 - 2008 The Regents of the University of California.
+# Copyright (c) 2000 - 2009 The Regents of the University of California.
 # All rights reserved.	
 # 
 # Redistribution and use in source and binary forms, with or without
@@ -54,6 +54,9 @@
 # @Copyright@
 #
 # $Log: __init__.py,v $
+# Revision 1.42  2009/05/01 19:07:34  mjk
+# chimi con queso
+#
 # Revision 1.41  2009/04/14 16:12:17  bruno
 # push towards chimmy beta
 #
